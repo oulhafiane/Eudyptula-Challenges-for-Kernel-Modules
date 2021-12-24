01374bb4f7d84104e60bdc1e3fd127818ccd9e23
Zakariaa Oulhafiane <zakariaa.oulhafiane@gmail.com>
2021-12-24 11:42:01 +0100

    Changed names of directories from ex.. to assignment_..

--- assignment_04/main.c
+++ assignment_04/main.c
@@ -10,20 +10,32 @@ MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Zakariaa Oulhafiane <zoulhafi@student.1337.ma>");
 MODULE_DESCRIPTION("Assignment 04");
 
+static int keyboard_probe(struct usb_interface *interface, const struct usb_device_id *id)
+{
+	printk(KERN_INFO "My Keyboard (%04X:%04X) plugged\n", id->idVendor, id->idProduct);
+	return 0;
+}
+
+static void keyboard_disconnect(struct usb_interface *interface)
+{
+    printk(KERN_INFO "My Keyboard removed\n");
+}
+
 static struct usb_device_id keyboard_table[] = {
 	{
-		USB_INTERFACE_INFO
-		(
-			USB_INTERFACE_CLASS_HID,
-			USB_INTERFACE_SUBCLASS_BOOT,
-			USB_INTERFACE_PROTOCOL_KEYBOARD
-		)
+	       USB_INTERFACE_INFO(
+			       USB_INTERFACE_CLASS_HID,
+			       USB_INTERFACE_SUBCLASS_BOOT,
+			       USB_INTERFACE_PROTOCOL_KEYBOARD
+			       )
 	}
 };
 
 static struct usb_driver keyboard_driver = {
 	.name = "mykeyboard_driver_assignment_04",
-	.id_table = keyboard_table
+	.id_table = keyboard_table,
+	.probe = keyboard_probe,
+	.disconnect = keyboard_disconnect
 };
 
 static int __init hello_init(void)
